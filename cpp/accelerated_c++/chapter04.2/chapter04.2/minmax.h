bd13f1f0fea4ff233d369e16ff11fb634e2089ba
zhouzhiqi <zhouzhiqi1234@outlook.com>
2020-04-18 22:37:28 +0800

    转成 VS 2019 项目

---
+++ cpp/accelerated_c++/chapter04.2/chapter04.2/minmax.h
@@ -0,0 +1,11 @@
+#ifndef MINMAX_H
+#define MINMAX_H
+
+#ifndef min
+#define min(a,b)  (((a) < (b)) ? (a) : (b))
+#endif
+#ifndef max
+#define max(a,b)  (((a) > (b)) ? (a) : (b))
+#endif
+
+#endif // MINMAX_H
